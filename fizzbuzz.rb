17a8e7994519d20dff5100d301bbe7f0152ca5de
Saint-Jean Germila <germila@noukod.com>
2018-08-31 15:54:50 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,2 +1,18 @@
+
+def fizzbuzz(int)
+     int = 0
+     if int % 3 == 0
+        "Fizz"
+     elsif int % 5 == 0
+        "Buzz"
+     elsif int % 15 == 0
+       "FizzBuzz"
+     else
+     if int % 4 == 0
+       "nil"
+      end
+ end
+
+
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
