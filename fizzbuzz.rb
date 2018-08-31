b7d595137d1a5edcf5a26f3f8f94aec3070c8abb
Saint-Jean Germila <germila@noukod.com>
2018-08-31 15:58:35 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -3,8 +3,8 @@ def fizzbuzz(int)
      int = 0
      if int % 3 == 0
          "Fizz"
-     #elsif int % 5 == 0
-      #  "Buzz"
+     elsif int % 5 == 0
+       "Buzz"
      #elsif int % 15 == 0
        #{}"FizzBuzz"
     # else
